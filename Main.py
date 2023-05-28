45f78898a77ca5d8b5372e8febf216001fdcbfd6
sharonsalman <sharon.salman1@gmail.com>
2023-05-28 13:29:03 +0300

    getting html class and id

--- Main.py
+++ Main.py
@@ -5,6 +5,8 @@ from selenium.webdriver import chrome
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 options = webdriver.ChromeOptions()
@@ -16,3 +18,14 @@ search = driver.find_element(By.ID, 'SearchString')
 search.send_keys("חיפה")
 search_box = driver.find_element(By.ID, 'submitSearchBtn')
 search_box.send_keys(Keys.RETURN)
+print(driver.page_source)
+try:
+    marging_container = WebDriverWait(driver , 15).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "marging-container"))
+    )
+    print(marging_container.page_source)
+except:
+    driver.quit()
+
+
+driver.quit()
