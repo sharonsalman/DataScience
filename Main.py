7007138dd82253134b6bf69ee8aa28458b48ddc4
sharonsalman <sharon.salman1@gmail.com>
2023-05-28 12:38:29 +0300

    google search

--- Main.py
+++ Main.py
@@ -1,10 +1,20 @@
 ﻿import selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import chrome
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options , service=Service(ChromeDriverManager().install()))
-driver.get('http://www.google.com/')
+driver.get('https://www.google.com/')
+
+search = driver.find_element(By.NAME, 'q')
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+time.sleep(5)
+
+driver.quit()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DataScience
+
+Hello! this is Sharon & Lihi's project about real astate in Israel.
+What affects the market price and why?
